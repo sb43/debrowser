bfaa10dee441342152ad1620fb1b09684885a1ec
Alper Kucukural <alper.kucukural@umassmed.edu>
2017-02-06 17:49:09 -0500

    css changes

--- R/downloadSection.R
+++ R/downloadSection.R
@@ -18,18 +18,20 @@ getDownloadSection <- function(flag = FALSE, choices=NULL) {
     a <- NULL
     if (flag){
         a <- list(conditionalPanel( (condition <- "input.methodtabs!='panel0'"),
-            selectInput("dataset", "Choose a dataset:",
-            choices = choices), 
-            selectInput("norm_method", "Normalization Method:",
-                choices <- c("TMM", "RLE", "upperquartile", "none")),
-            downloadButton("downloadData", "Download Data"),
-            conditionalPanel(condition = "input.dataset=='most-varied'",
-            textInput("topn", "top-n", value = "500" ), 
-            textInput("mincount", "total min count", value = "10" )),
-            textareaInput("genesetarea","Search", 
-                          "", rows = 5, cols = 35),
-            helpText("Regular expressions can be used\n
-                     Ex: ^Al => Al.., Al$ => ...al")
+                shinydashboard::menuItem("Select Plot Options", icon = icon("star-o"),                       
+                    selectInput("dataset", "Choose a dataset:",
+                    choices = choices), 
+                    selectInput("norm_method", "Normalization Method:",
+                        choices <- c("TMM", "RLE", "upperquartile", "none")),
+                    downloadButton("downloadData", "Download Data"),
+                    conditionalPanel(condition = "input.dataset=='most-varied'",
+                    textInput("topn", "top-n", value = "500" ), 
+                    textInput("mincount", "total min count", value = "10" )),
+                    textareaInput("genesetarea","Search", 
+                                  "", rows = 5, cols = 35),
+                    helpText("Regular expressions can be used\n
+                             Ex: ^Al => Al.., Al$ => ...al")
+                )
             ))
     }
     a
--- R/ui.R
+++ R/ui.R
@@ -12,27 +12,53 @@
 #'
 
 deUI <- function() {
-    heatmapJScode <- 
+
+    heatmapJScode <-
         "shinyjs.getNames = function(){
-        var count = document.getElementsByClassName('tick').length; 
-        var start = 0;  
+        var count = document.getElementsByClassName('tick').length;
+        var start = 0;
         while(document.getElementsByClassName('tick')[start].getElementsByTagName('line')[0].getAttribute('x2') == 0){
         start += 1;
-        }  
-        var out = ''; 
-        for (i = start; i < count; i++) 
-        { 
-        if('opacity: 1;' == document.getElementsByClassName('tick')[i].getAttribute('style')){ 
+        }
+        var out = '';
+        for (i = start; i < count; i++)
+        {
+        if('opacity: 1;' == document.getElementsByClassName('tick')[i].getAttribute('style')){
         out += document.getElementsByClassName('tick')[i].getElementsByTagName('text')[0].innerHTML + ',';
         }
-        } 
+        }
         //document.getElementById('genenames').innerHTML = out;
         Shiny.onInputChange('genenames', out);
     };"
-    addResourcePath(prefix = "www", directoryPath =
-        system.file("extdata", "www", 
-        package = "debrowser"))
-    shinyUI(fluidPage(
+    dbHeader <- shinydashboard::dashboardHeader(titleWidth = 350)
+    dbHeader$children[[2]]$children <-  tags$a(style='color: white;',
+        href = "/" , "DEBrowser")
+    
+    addResourcePath(prefix = "www", directoryPath = system.file("extdata",
+        "www", package = "debrowser"))
+
+    
+    if(!file.exists("shiny_saves/startup.rds")){
+        startup_obj <- list()
+        startup_obj$bookmark_counter <- 3
+        startup_obj$startup_bookmark <- ""
+        dir.create("shiny_saves")
+        saveRDS(startup_obj, "shiny_saves/startup.rds")
+    }        
+        
+    startup <- readRDS("shiny_saves/startup.rds")
+    
+    if (startup[['bookmark_counter']] == 0){
+        a <- (fluidPage(
+            tags$script('Shiny.addCustomMessageHandler("testmessage",
+                function(message) {
+                    window.location.href = message.bookmarked_url;
+                }
+        );') ))
+    }
+
+    else{
+    a <- (fluidPage(
     shinyjs::useShinyjs(),
     shinyjs::extendShinyjs(text = heatmapJScode, functions = c("getNames")),
     shinyjs::inlineCSS("
@@ -46,44 +72,66 @@ deUI <- function() {
         height: 100%;
         text-align: center;
         color: #EFEFEF;
-}"),
+    }"),
     # Loading message
     tags$div(h4("Loading DEBrowser"), id = "loading-debrowser",
         tags$img(src = "www/images/initial_loading.gif")),
-    uiOutput("logo"),
-    uiOutput("programtitle"),
-    textOutput("text"),
-    sidebarLayout(
-        sidebarPanel(
-        uiOutput("loading"),
-        width = 2,
-        uiOutput("initialmenu"),
-        conditionalPanel(condition = "(output.dataready)",
-            uiOutput("leftMenu")),
-        conditionalPanel(condition = "(output.dataready)",
-            uiOutput("downloadSection")),
-        conditionalPanel(condition = "(output.dataready)",
-            uiOutput('cutoffSelection'))
-    ),
-    mainPanel(
     tags$head(
-    tags$style(type = "text/css",
-                 "#methodtabs.nav-tabs {font-size: 14px} ")),
-    tabsetPanel(id = "methodtabs", type = "tabs",
+        tags$link(rel = "stylesheet", type = "text/css",
+                  href = "www/shinydashboard_additional.css")
+    ),
+    shinydashboard::dashboardPage(
+        dbHeader,
+        shinydashboard::dashboardSidebar(
+            width = 350,
+            uiOutput("loading"),
+            uiOutput("initialmenu"),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput("leftMenu")),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput("downloadSection")),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput('cutoffSelection')),
+            conditionalPanel(condition = paste0("((input.goDE) || ",
+                "(output.restore_DE > 0)) && (!input.startDE)"),
+                style = "padding: 27px;",
+            actionButton("save_state", "Save State!"),
+            conditionalPanel(condition = "input.save_state",
+                textInput("bookmark_special_name", "Name your save:",
+                    value = "", placeholder = "At Least 5 Characters"),
+                    actionButton("name_bookmark", "Submit!"),
+                    textOutput("bookmark_length_error"),
+                    br(), br(), br()
+            )),
+            conditionalPanel(condition <- paste0("input.methodtabs=='panel0'"),
+                htmlOutput("new_bookmark"),
+                htmlOutput("past_named_bookmarks")
+            )
+    ),
+    shinydashboard::dashboardBody(
+        mainPanel(
+            width = 10,
+            tags$head(
+                tags$style(type = "text/css",
+                        "#methodtabs.nav-tabs {font-size: 14px} ")),
+            tabsetPanel(id = "methodtabs", type = "tabs",
                 tabPanel(title = "Data Prep", value = "panel0", id="panel0",
-                         uiOutput("preppanel")),
+                        uiOutput("preppanel")),
                 tabPanel(title = "Main Plots", value = "panel1", id="panel1",
-                         uiOutput("mainmsgs"),
-                         conditionalPanel(condition = "input.demo || 
-                             output.dataready", uiOutput("mainpanel"))),
+                        uiOutput("mainmsgs"),
+                        conditionalPanel(condition = "input.demo ||
+                        output.dataready", uiOutput("mainpanel"))),
                 tabPanel(title = "QC Plots", value = "panel2", id="panel2",
-                         uiOutput("qcpanel")),
+                        uiOutput("qcpanel")),
                 tabPanel(title = "GO Term", value = "panel3", id="panel3",
-                         uiOutput("gopanel")),
+                        uiOutput("gopanel")),
                 tabPanel(title = "Tables", value = "panel4", id="panel4",
-                         DT::dataTableOutput("tables")))
-        ))
-    ))
+                        DT::dataTableOutput("tables")))
+                )
+            )
+            )
+        )
+    )
+    }
+    a
 }
-
-
