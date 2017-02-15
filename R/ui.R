2a2d6ac1ef6d849ac722c505c0ea5692b3417c44
Alper Kucukural <alper.kucukural@umassmed.edu>
2017-02-14 20:43:12 -0500

    Revert "google extra changes"

--- R/ui.R
+++ R/ui.R
@@ -32,9 +32,9 @@ enableBookmarking("server")
         Shiny.onInputChange('genenames', out);
     };"
     dbHeader <- shinydashboard::dashboardHeader(titleWidth = 350)
-    # dbHeader$children[[2]]$children <- tags$a(style='color: white;',
-    #     href = paste0("/") , "DEBrowser")
-    dbHeader$children[[2]]$children <- "DEBrowser"
+    dbHeader$children[[2]]$children <- tags$a(style='color: white;',
+        href = paste0("/") , "DEBrowser")
+
     
     addResourcePath(prefix = "www", directoryPath = system.file("extdata",
         "www", package = "debrowser"))
@@ -83,41 +83,37 @@ enableBookmarking("server")
         dbHeader,
         shinydashboard::dashboardSidebar(
             width = 350,
-            conditionalPanel(condition = "!output.user_name",
-                             googleAuthUI("example1")),
-            conditionalPanel(condition = "output.user_name",
-                a("Full Refresh", href="/", class="btn btn-primary", role="button"),
-                uiOutput("loading"),
-                uiOutput("initialmenu"),
-                conditionalPanel(condition = "(output.dataready)",
-                    uiOutput("leftMenu")),
-                conditionalPanel(condition = "(output.dataready)",
-                    uiOutput("downloadSection")),
-                conditionalPanel(condition = "(output.dataready)",
-                    uiOutput('cutoffSelection')),
-                conditionalPanel(condition = paste0("((input.goDE) || ",
-                    "(output.restore_DE > 0)) && (!input.startDE)"),
-                    style = "padding: 27px;",
+            googleAuthUI("example1"),
+            p("Logged in as: ", textOutput("user_name")),
+            uiOutput("loading"),
+            uiOutput("initialmenu"),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput("leftMenu")),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput("downloadSection")),
+            conditionalPanel(condition = "(output.dataready)",
+                uiOutput('cutoffSelection')),
+            conditionalPanel(condition = paste0("((input.goDE) || ",
+                "(output.restore_DE > 0)) && (!input.startDE)"),
+                style = "padding: 27px;",
+            
+            actionButton("save_state", "Save Selection!"),
+            #textOutput("message_for_loading"),
+            conditionalPanel(condition = "input.save_state",
+                textInput("bookmark_special_name", "Name your save:",
+                    value = "", placeholder = "At Least 5 Characters"),
+                    actionButton("name_bookmark", "Submit!"),
                 
-                actionButton("save_state", "Save Selection!"),
-                #textOutput("message_for_loading"),
-                conditionalPanel(condition = "input.save_state",
-                    textInput("bookmark_special_name", "Name your save:",
-                        value = "", placeholder = "At Least 5 Characters"),
-                        actionButton("name_bookmark", "Submit!"),
-                    
-                        textOutput("bookmark_length_error"),
-    
-                        br(), br(), br()
-                )),
-                conditionalPanel(condition <- paste0("input.methodtabs=='panel0'"),
-                    htmlOutput("new_bookmark"),
-                    uiOutput("past_named_bookmarks"),
-                    lapply(20:1, function(i) {
-                        uiOutput(paste0('bookmark', i))
-                    })
-                ), br(), br(),
-                p("Logged in as: ", textOutput("user_name"))
+                    textOutput("bookmark_length_error"),
+
+                    br(), br(), br()
+            )),
+            conditionalPanel(condition <- paste0("input.methodtabs=='panel0'"),
+                htmlOutput("new_bookmark"),
+                uiOutput("past_named_bookmarks"),
+                lapply(20:1, function(i) {
+                    uiOutput(paste0('bookmark', i))
+                })
             )
     ),
     shinydashboard::dashboardBody(
@@ -126,22 +122,22 @@ enableBookmarking("server")
             tags$head(
                 tags$style(type = "text/css",
                         "#methodtabs.nav-tabs {font-size: 14px} ")),
-            conditionalPanel(condition = "output.user_name",
-                tabsetPanel(id = "methodtabs", type = "tabs",
-                    tabPanel(title = "Data Prep", value = "panel0", id="panel0",
-                            uiOutput("preppanel")),
-                    tabPanel(title = "Main Plots", value = "panel1", id="panel1",
-                            uiOutput("mainmsgs"),
-                            conditionalPanel(condition = "input.demo ||
-                            output.dataready", uiOutput("mainpanel"))),
-                    tabPanel(title = "QC Plots", value = "panel2", id="panel2",
-                            uiOutput("qcpanel")),
-                    tabPanel(title = "GO Term", value = "panel3", id="panel3",
-                            uiOutput("gopanel")),
-                    tabPanel(title = "Tables", value = "panel4", id="panel4",
-                            DT::dataTableOutput("tables")))
+            tabsetPanel(id = "methodtabs", type = "tabs",
+                tabPanel(title = "Data Prep", value = "panel0", id="panel0",
+                        uiOutput("preppanel")),
+                tabPanel(title = "Main Plots", value = "panel1", id="panel1",
+                        uiOutput("mainmsgs"),
+                        conditionalPanel(condition = "input.demo ||
+                        output.dataready", uiOutput("mainpanel"))),
+                tabPanel(title = "QC Plots", value = "panel2", id="panel2",
+                        uiOutput("qcpanel")),
+                tabPanel(title = "GO Term", value = "panel3", id="panel3",
+                        uiOutput("gopanel")),
+                tabPanel(title = "Tables", value = "panel4", id="panel4",
+                        DT::dataTableOutput("tables")))
+                )
             )
-        )))
+            )
         )
     )
     }
