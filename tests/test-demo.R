33f6fd56c7040f70a5df666606bfb746a8f6ec0d
Alper Kucukural <alper.kucukural@umassmed.edu>
2018-10-27 17:27:09 -0400

    Last publicatin fixes

--- tests/test-demo.R
+++ tests/test-demo.R
@@ -6,8 +6,8 @@ test_that("demo data can be loaded", {
     load(system.file("extdata", "demo", "demodata.Rda",
         package = "debrowser"))
     expect_true(is.data.frame(demodata))
-    expect_equal(demodata[29311, 2], 7.1)
-    expect_equal(demodata[29311, 5], 2)
+    expect_equal(demodata[29311, 2], 2)
+    expect_equal(demodata[29311, 5], 7.1)
     expect_equal(demodata[29311, 6], 6)
     expect_null(demodata[1, 7])
 })
